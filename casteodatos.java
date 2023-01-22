d493caaadb7e6a7ec8a157880353c6770cf7fc26
juanpablo2025 <juanpablo_1810dev@hotmail.com>
2023-01-22 15:03:25 -0500

    casteodedatos JAVA

---
+++ casteodatos.java
@@ -0,0 +1,49 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author 503
+ */
+public class casteodatos {
+
+	/**
+	 * @param args the command line arguments
+	 */
+
+	float variable1 = (float) 2.58;
+	double varible6 = Double.valueOf(variable1);
+	int varible7 = (int) variable1;
+	short variable8 = (short) variable1;
+	byte variable9 = (byte) variable1;
+	// ---------------------------------------------------
+	int variable2 = 256;
+	double variable10 = Double.valueOf(variable2);
+	short variable11 = (short) variable2;
+	byte variable12 = (byte) variable2;
+	// --------------------------------------------------
+	double variable3 = -47.56487;
+	float variable13 = (float) variable3;
+	int variable14 = (int) (variable3);
+	short variable15 = (short) variable3;
+	byte variable16 = (byte) variable3;
+	// -------------------------------------------------
+
+	short variable4 = -10;
+	float variable17 = (float) variable4;
+	int variable18 = (int) variable4;
+	byte variable19 = (byte) variable4;
+
+	byte variable5 = (byte) 128;
+	float variable20 = (float) variable5;
+	int variable21 = (int) variable5;
+	long variable22 = (long) variable5;
+
+	public static void main(String[] args) {
+
+	}
+}
